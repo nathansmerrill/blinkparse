c649b81ce133a069f491a95d865caba9ef895086
Nathan Merrill <mathiscool3000@gmail.com>
2020-02-01 13:53:41 +0000

    Update setup.py github link

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A python library for parsing command line arguments",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/nathansmerrill/cliparse2",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
