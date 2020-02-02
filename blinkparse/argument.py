8944892dd50052bba2cb9c724a85d2d0f7c7421d
Nathan Merrill <mathiscool3000@gmail.com>
2020-02-02 13:07:42 +0000

    Add operand parsing

--- blinkparse/argument.py
+++ blinkparse/argument.py
@@ -8,11 +8,11 @@ class Argument:
 
     def check(self, inputArgs):
         outputArgs = {}
+
         for i, inputArg in enumerate(inputArgs):
             if f'--{self.name}' in inputArg:
                 outputArgs[self.name] = None
                 splitArg = inputArg.split('=')
-                inputArgs.pop(i)
                 if self.takesValue:
                     if len(splitArg) == 2:
                         outputArgs[self.name] = splitArg[1]
@@ -21,7 +21,9 @@ class Argument:
                 else:
                     if len(splitArg) != 1:
                         raise ValueError(f'The {self.name} argument doesn\'t take input. Did you mean --{self.name}')
+                inputArgs.pop(i)
                 break
+
             elif self.shortName is not None and inputArg == f'-{self.shortName}':
                 outputArgs[self.name] = None
                 if self.takesValue:
@@ -30,7 +32,11 @@ class Argument:
                     except IndexError:
                         raise ValueError(f'The {self.name} argument takes input. Did you mean -{self.shortName} myValue')
                     outputArgs[self.name] = value
+                inputArgs.pop(i)
+                inputArgs.pop(i)
                 break
+
         if self.required and len(outputArgs) == 0:
             raise ValueError(f'The {self.name} argument is required')
+
         return outputArgs
